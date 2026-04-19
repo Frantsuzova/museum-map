c3a2685566f449079eb1b64d6c475dff864d2194
Anna Chizhik <53980712+Frantsuzova@users.noreply.github.com>
2026-04-19 23:38:08 +0300

    Initialize museum_map module with exports

---
+++ museum_map/__init__.py
@@ -0,0 +1,8 @@
+from .config import MuseumMapConfig
+from .pipeline import MuseumMapPipeline, build_museum_map
+
+__all__ = [
+    "MuseumMapConfig",
+    "MuseumMapPipeline",
+    "build_museum_map",
+]
